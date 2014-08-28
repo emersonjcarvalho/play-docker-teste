dcdbb93d964ff631bc1e75163d21a9a39dd8f2c5
sistemata <emersonjcarvalho@gmail.com>
2014-08-28 13:52:48 -0300

    target zip file

---
+++ target/scala-2.11/src_managed/main/controllers/routes.java
@@ -0,0 +1,23 @@
+// @SOURCE:W:/GitHub/play-docker-teste/conf/routes
+// @HASH:6a60b84db1de7d7f0ae39cab42b77f63e7adfb54
+// @DATE:Thu Aug 28 13:51:11 GMT-03:00 2014
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+          
+
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+          
+}
+          
